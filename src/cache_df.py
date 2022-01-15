48c0dd161be9b2faa45c3a24bd71803534be455c
Kevin Wood <kevin.wood75@gmail.com>
2022-01-15 00:16:26 -0500

    added portfolio adder

--- src/cache_df.py
+++ src/cache_df.py
@@ -1,11 +1,24 @@
 import pyarrow as pa
 import redis
 import yfinance as yf
+import psycopg2
 import sys
 import warnings
 warnings.filterwarnings("ignore")
 
-symbols = sys.argv[1]
+# symbols = sys.argv[1]
+
+def get_data_db(query):
+    connection = psycopg2.connect(user="stockfi",
+                                  password="jandrew28",
+                                  host="postgresdb1.woodez.net",
+                                  port="5432",
+                                  database="stockfi")
+    cursor = connection.cursor()
+    cursor.execute(query)
+    records = cursor.fetchall()
+    return records
+
 
 
 def cache_df(alias,df):
@@ -29,8 +42,10 @@ def import_data_redis(alias,stock_symbol):
 
 # symbols = "HOOD,ETH-CAD,BTC-CAD,UBER,TSLA,SBUX,BAC,HHC,PLTR,BYL.TO,BBD-B.TO,NOK,NPI.TO,GILD,DAL,SNAP,DIS,MSFT,SQ,AER,WYNN"
 
-
-for tick in symbols.split(","):
+records = get_data_db("select * from mybag_mybag")
+for row in records:
+    tick = row[2]
+#for tick in symbols.split(","):
     if '-' in tick:
        alias = tick.split("-")[0].upper()
     elif '.' in tick:
