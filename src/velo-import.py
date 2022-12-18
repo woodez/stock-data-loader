e964f796b0f424f60d46b8922b8a29fee717af9b
Kevin Wood <kevin.wood75@gmail.com>
2022-12-17 20:58:28 -0500

    test

---
+++ src/velo-import.py
@@ -0,0 +1,9 @@
+# Python program to illustrate
+# creating a data frame using CSV files
+ 
+# import pandas module
+import pandas as pd
+ 
+# creating a data frame
+df = pd.read_csv("Braydon-velo-measure.csv")
+print(df['Crowhop'.*])
